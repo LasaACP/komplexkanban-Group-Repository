636feb1f221faefb10c4c67b479717b0634bf899
Jai Nagaraj <jai.v.nagaraj@gmail.com>
2022-12-12 20:54:26 -0600

    Created trig tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -45,45 +45,45 @@ TEST_CASE("Addition", "[Complex]")
 {
   cout << "Running tests on addition:" << endl;
   REQUIRE(((complex(1,2) + complex(4,5)) == complex(5,7)) == true);
-  REQUIRE((complex(1,2) + 2 == complex(3,2))==true);
+  REQUIRE((complex(1,2) + 2 == complex(3,2)) == true);
   REQUIRE((complex(1,2) + complex(0,0) == complex(1,2)) == true);
 }
 
 TEST_CASE("Subtraction", "[Complex]")
 {
   cout << "Running tests on subtraction" << endl;
-  REQUIRE((complex(1,2) - complex(4,5) == complex(-3,-3))==true);
-  REQUIRE((complex(1,2) - 2 == complex(-1,2))==true);
-  REQUIRE((complex(1,2) - complex(0,0) == complex(1,2))==true);
+  REQUIRE((complex(1,2) - complex(4,5) == complex(-3,-3)) == true);
+  REQUIRE((complex(1,2) - 2 == complex(-1,2)) == true);
+  REQUIRE((complex(1,2) - complex(0,0) == complex(1,2)) == true);
 }
 
 TEST_CASE("Multiplication", "[Complex]")
 {
   cout << "Running tests on multiplication" << endl;
-  REQUIRE((complex(1,2) * complex(4,5) == complex(-6,13)) ==true);
-  REQUIRE((complex(1,2) * 2 == complex(2,4)) ==true);
-  REQUIRE((complex(1,2) * complex(0,0) == complex(0,0)) ==true);
+  REQUIRE((complex(1,2) * complex(4,5) == complex(-6,13)) == true);
+  REQUIRE((complex(1,2) * 2 == complex(2,4)) == true);
+  REQUIRE((complex(1,2) * complex(0,0) == complex(0,0)) == true);
 }
 
 TEST_CASE("Division", "[Complex]")
 {
   cout << "Running tests on division" << endl;
-  REQUIRE((complex(20,-4) / complex(3,2) == complex(4,-4)) ==true);
-  REQUIRE((complex(1,2) / 2 == complex(0.5,1)) ==true);
+  REQUIRE((complex(20,-4) / complex(3,2) == complex(4,-4)) == true);
+  REQUIRE((complex(1,2) / 2 == complex(0.5,1)) == true);
 }
 
 TEST_CASE("Real", "[Complex]")
 {
   cout << "Running tests on real" << endl;
   complex c = complex(20,1);
-  REQUIRE(c.real(c) == 20);
+  REQUIRE((c.real(c) == 20) == true);
 }
 
 TEST_CASE("Imag", "[Complex]")
 {
   cout << "Running tests on imag" << endl;
   complex c = complex(20,1);
-  REQUIRE(c.imag(c) == 1);
+  REQUIRE((c.imag(c) == 1) == true);
 }
 
 TEST_CASE("Conj", "[Complex]")
@@ -91,7 +91,7 @@ TEST_CASE("Conj", "[Complex]")
   cout << "Running tests on conj" << endl;
   complex c = complex(20,1);
   complex answer = complex(20,-1);
-  REQUIRE((c.conj(c)==answer) ==true);
+  REQUIRE((c.conj(c)==answer) == true);
 }
 
 TEST_CASE("==", "[Complex]")
@@ -101,10 +101,10 @@ TEST_CASE("==", "[Complex]")
   complex d = complex(20,1);
   complex e = complex(19,0);
   double f = 19;
-  REQUIRE(((c==d)) ==true);
-  REQUIRE((!(c==e)) ==true);
-  REQUIRE((e==f) ==true);
-  REQUIRE((f==e) ==true);
+  REQUIRE(((c==d)) == true);
+  REQUIRE((!(c==e)) == true);
+  REQUIRE((e==f) == true);
+  REQUIRE((f==e) == true);
 }
 
 TEST_CASE("!=", "[Complex]")
@@ -114,10 +114,10 @@ TEST_CASE("!=", "[Complex]")
   complex d = complex(20,1);
   complex e = complex(19,0);
   double f = 18;
-  REQUIRE(((c!=e)) ==true);
-  REQUIRE((!(c!=d)) ==true);
-  REQUIRE((e!=f) ==true);
-  REQUIRE((f!=e) ==true);
+  REQUIRE(((c!=e)) == true);
+  REQUIRE((!(c!=d)) == true);
+  REQUIRE((e!=f) == true);
+  REQUIRE((f!=e) == true);
 }
 
 TEST_CASE("Pow", "[Complex]")
@@ -129,10 +129,64 @@ TEST_CASE("Pow", "[Complex]")
   complex answer2 = complex(-7.4614966,2.8854927);
   complex answer3 = complex(10.7973867,27.3083775);
   complex answer4 = complex(-0.2599533,0.5545326);
-  REQUIRE((pow(c,2)==answer) ==true);
-  REQUIRE((pow(2,c)==answer2) ==true);
-  REQUIRE((pow(c,2.1)==answer3) ==true);
-  REQUIRE((pow(c,exponent)==answer4) ==true);
+  REQUIRE((pow(c,2)==answer) == true);
+  REQUIRE((pow(2,c)==answer2) == true);
+  REQUIRE((pow(c,2.1)==answer3) == true);
+  REQUIRE((pow(c,exponent)==answer4) == true);
+}
+
+TEST_CASE("Sine", "[Complex]")
+{
+  cout << "Running tests on sine:" << endl;
+  REQUIRE((sin(complex(1,2)) == complex(3.16577851,1.95960104)) == true);
+}
+
+TEST_CASE("Cosine", "[Complex]")
+{
+  cout << "Running tests on cosine:" << endl;
+  REQUIRE((cos(complex(1,2)) == complex(2.03272301,-3.0518978)) == true);
+}
+
+TEST_CASE("Tangent", "[Complex]")
+{
+  cout << "Running tests on tangent:" << endl;
+  REQUIRE((tan(complex(1,2)) == complex(0.03381283,1.01479362)) == true);
+}
+
+TEST_CASE("Hyperbolic Sine", "[Complex]")
+{
+  cout << "Running tests on hyperbolic sine:" << endl;
+  REQUIRE((sinh(complex(1,2)) == complex(-0.48905626,1.40311925)) == true);
+}
+
+TEST_CASE("Hyperbolic Cosine", "[Complex]")
+{
+  cout << "Running tests on hyperbolic cosine:" << endl;
+  REQUIRE((cosh(complex(1,2)) == complex(-0.64214812,1.06860742)) == true);
+}
+
+TEST_CASE("Hyperbolic Tangent", "[Complex]")
+{
+  cout << "Running tests on hyperbolic tangent:" << endl;
+  REQUIRE((tanh(complex(1,2)) == complex(1.16673626,-0.2434582)) == true);
+}
+
+TEST_CASE("Inverse Sine", "[Complex]")
+{
+  cout << "Running tests on inverse sine:" << endl;
+  REQUIRE((asin(complex(1,2)) == complex(0.42707859,1.52857092)) == true);
+}
+
+TEST_CASE("Inverse Cosine", "[Complex]")
+{
+  cout << "Running tests on inverse cosine:" << endl;
+  REQUIRE((acos(complex(1,2)) == complex(1.14371774,-1.52857092)) == true);
+}
+
+TEST_CASE("Inverse Tangent", "[Complex]")
+{
+  cout << "Running tests on inverse tangent:" << endl;
+  REQUIRE((atan(complex(1,2)) == complex(1.33897252,0.40235948)) == true);
 }
 
 // */
